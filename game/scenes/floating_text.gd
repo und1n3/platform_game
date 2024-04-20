3787e33160d927aaf94f1cf450fca09918c6c1f1
und1n3 <undineunda5@gmail.com>
2024-04-20 17:53:15 +0200

    added collision

--- game/scenes/floating_text.gd
+++ game/scenes/floating_text.gd
@@ -3,9 +3,14 @@ extends Node3D
 @export var offset = Vector3(0, 0.7, 0)
 @export var duration = 2.0
 @export var text = ""
+@export var level_file : PackedScene = null
 
 func _ready():
 	start_tween()
+	
+func player_collision():
+	if level_file != null:
+		get_tree().change_scene_to_packed(level_file)
 
 func start_tween():
 	$Text.mesh.text = text
